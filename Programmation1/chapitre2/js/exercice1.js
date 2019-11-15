c0653c408b6224fd12749c0641488b003547e73e
MichaelAhKam <michaelahkamgithub@gmail.com>
2019-11-14 21:01:38 -0500

    Envoi chapitre2

---
+++ Programmation1/chapitre2/js/exercice1.js
@@ -0,0 +1,9 @@
+//Lire deux nombres et afficher le plus grand des deux.
+
+var nb1
+var nb2
+
+nb1 = Number(prompt("Veuillez entrer un nombre svp : "));
+nb2 = Number(prompt("Veuillez entrer un nombre svp : "));
+
+document.write("Le nombre le plus grand est " + nb1)
