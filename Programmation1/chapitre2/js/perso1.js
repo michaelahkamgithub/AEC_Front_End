cde517526798501e7675be54e5bffb12784ae880
MichaelAhKam <michaelahkamgithub@gmail.com>
2019-11-15 23:34:47 -0500

    Envoie chapitre2

--- Programmation1/chapitre2/js/perso1.js
+++
@@ -1,8 +0,0 @@
-var montant=Number(prompt('Entrer votre montant:'));
-
-if (montant >= 200){
-    document.write('Vous obtenez 15% de remise');
-}
-else{
-    alert("Vous n'obtenez pas de rabais");
-}
