5168eac8149ddeae5d808d47d5bf9f5c6c746ae5
MichaelAhKam <michaelahkamgithub@gmail.com>
2019-11-17 21:41:41 -0500

    Mise à jour exercice chapitre 1

--- Programmation1/chapitre1/js/exercice14.js
+++ Programmation1/chapitre1/js/exercice14.js
@@ -1,3 +1,4 @@
+var nomEtudiant;
 var note1;
 var note2;
 var note3;
@@ -5,9 +6,10 @@ var noteFinal;
 
 
 //Étape 2 : Lecture (demander à l'usager des données)
-note1 = Number(prompt("Entrer votre note de mi-session "));
-note2 = Number(prompt("Entrer votre note de l'examen final "));
-note3 = Number(prompt("Entrer votre note des laboratoires "));
+nomEtudiant = prompt("Entrer votre nom");
+note1 = Number(prompt("Entrer votre note de mi-session (compte pour 30%) "));
+note2 = Number(prompt("Entrer votre note de l'examen final (compte pour 50%) "));
+note3 = Number(prompt("Entrer votre note des laboratoires (compte pour 20%) "));
 
 
 
@@ -15,6 +17,10 @@ note3 = Number(prompt("Entrer votre note des laboratoires "));
 noteFinal = (note1 * 30) / 100 + (note2 * 20) / 100 + (note3 * 50) / 100;
 
 //Étape 4 : AFFICHE les résultats
+alert("Nom de l'étudiant : " + nomEtudiant);
+alert("Votre note de mi-session qui compte pour 30% est de : " + note1 + " sur 100");
+alert("Votre note de l'examen final qui compte pour 50% est de : " + note2 + " sur 100");
+alert("Votre note des laboratoires qui compte pour 20% est de : " + note3 + " sur 100");
 alert("Votre note final est de : " + noteFinal + "%");
 
 
