5246e0af027f5ec100e006a9813c7e08e2346954
MichaelAhKam <michaelahkamgithub@gmail.com>
2019-11-25 19:26:20 -0500

    Envoie du chapitre 2

--- Programmation1/chapitre2/js/exercice5.js
+++ Programmation1/chapitre2/js/exercice5.js
@@ -6,8 +6,11 @@ var temp1;
 temp1 = Number(prompt("Entrer la température "));
 
 
-if(temp1 <= -10){
+if(temp1 >= -40 && temp1 <= -10){
     alert('HAAAAAAA! Il fait froid');
+}
+else if(temp1 < -40){
+    alert("C'est pas humain");
 }
 else{
     alert("Enfin une belle journée");
